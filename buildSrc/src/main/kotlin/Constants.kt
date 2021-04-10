0e31a1244507c4505b82695564f2f4e10852721b
Alessandro Oddone <38383716+AlessandroOddone@users.noreply.github.com>
2021-04-10 12:10:00 -0500

    Kotlin 1.4.32 and KSP version bump (#12)

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,11 +1,11 @@
 object Versions {
   const val COROUTINES = "1.4.2"
   const val DOKKA = "1.4.30"
-  const val KOTLIN = "1.4.31"
+  const val KOTLIN = "1.4.32"
   const val KOTLIN_POET = "1.8.0"
   const val KOTLIN_COMPILE_TESTING = "1.3.6"
   const val KOTLINX_SERIALIZATION = "1.1.0"
-  const val KSP = "1.4.31-1.0.0-alpha06"
+  const val KSP = "1.4.32-1.0.0-alpha07"
   const val KTLINT_GRADLE_PLUGIN = "10.0.0"
   const val KTOR = "1.5.2"
   const val MAVEN_PUBLISH_PLUGIN = "0.14.2"
